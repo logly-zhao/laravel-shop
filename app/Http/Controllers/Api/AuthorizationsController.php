5f442a7e4968b6e9edd66275d13179159976a701
logly-zhao <ab52525258@sina.com>
2018-09-19 10:45:27 +0000

    add use head

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+//use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\WeappAuthorizationRequest;
 
 
